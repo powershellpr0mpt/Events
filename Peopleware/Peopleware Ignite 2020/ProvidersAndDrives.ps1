e144d47d8e7d0d1fb0713a707634a2f9527a6382
powershellpr0mpt <robert@powershellpr0mpt.com>
2020-03-26 22:00:07 +0100

    Peopleware Ignite 2020 added

---
+++ Peopleware/Peopleware Ignite 2020/ProvidersAndDrives.ps1
@@ -0,0 +1,36 @@
+﻿<# ProAct - Fear the Shell - or shouldn't you
+Robert Prüst
+@r_prust
+http://powershellpr0mpt.com
+#>
+
+#region PSProviders & PSDrives
+Get-Command -Noun PSProvider
+help Get-PSProvider
+Get-PSProvider
+
+Get-Alias cd
+Get-Alias -Definition Set-Location
+& regedit #help about_operators 
+cd Registry:
+
+Get-PSDrive -PSProvider Registry
+cd HKCU:
+Get-ChildItem .
+Get-Childitem 'Environment'
+Get-Item 'Environment'
+Get-ItemProperty 'Environment'
+
+New-Item -Path HKCU: -Name ProAct
+New-ItemProperty -Path HKCU:\ProAct -Name PowerShell -PropertyType String -Value 'Is awesome!'
+Remove-Item -Path HKCU:\ProAct
+
+Set-Location Scripts:
+
+Get-ChildItem Alias:
+Get-ChildItem Env:
+Get-ChildItem Function:
+Get-ChildItem Variable:
+#endregion
+
+
