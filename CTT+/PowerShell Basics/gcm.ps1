9a6419acb62a1f5827483d7b661e9e6ea15a2e39
Robert Prüst <robert@powershellpr0mpt.com>
2020-11-04 00:23:27 +0100

    Added CTT+ training material

---
+++ CTT+/PowerShell Basics/gcm.ps1
@@ -0,0 +1,12 @@
+# Get-Command
+
+## What does it do?
+Get-Command
+
+## How many do I have?
+(Get-Command).Count
+
+# How can I find what I'm looking for?
+Get-Command -Verb Get
+Get-Command -Noun Process
+Get-Command -Name *e*-*Process*
---
+++ CTT+/PowerShell Basics/whoami.ps1
@@ -0,0 +1,9 @@
+[PSCustomObject]@{
+    Name    = 'Robert Prüst'
+    Job     = 'Infrastructure Specialist'
+    Company = 'Proact'
+    Website = 'https://powershellpr0mpt.com'
+    GitHub  = 'https://github.com/powershellpr0mpt'
+    Mail    = 'robert@powershellpr0mpt.com'
+    Twitter = '@r_prust'
+}
